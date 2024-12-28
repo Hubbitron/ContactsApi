0783231eac112d63341a4d18f3e1ebace325e9ab
Hubbitron <hubbitron@gmail.com>
2024-12-28 18:39:17 -0500

    added state backl

---
+++ src/main/java/com/contacts/model/State.java
@@ -0,0 +1,25 @@
+package com.contacts.model;
+
+public class State {
+	long id;
+	String stateAbbr;
+	String stateName;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getStateAbbr() {
+		return stateAbbr;
+	}
+	public void setStateAbbr(String stateAbbr) {
+		this.stateAbbr = stateAbbr;
+	}
+	public String getStateName() {
+		return stateName;
+	}
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}	
+}
