205d4b6136b2d76120ebb8c53d6d32e98194249c
Hubbitron <hubbitron@gmail.com>
2025-01-01 11:18:23 -0500

    fixed jwt.util deprecated code

--- src/main/java/com/contacts/helper/JwtUtil.java
+++ src/main/java/com/contacts/helper/JwtUtil.java
@@ -1,10 +1,11 @@
 package com.contacts.helper;
 
-
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.crypto.SecretKey;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -22,106 +23,92 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
-public class JwtUtil
-{
+public class JwtUtil {
 	@Value("${jwt.auth.secret_key}")
 	private String secretKey;
 
 	@Value("${jwt.expiration.interval.minutes}")
 	private long jwtExpirationIntervalMinutes;
 
-    public String extractUsername(String token) throws MalformedJwtException, ExpiredJwtException
-    {
-    	Claims claims = extractAllClaims(token);
-    	if (claims == null)
-    		return null;
-    	
-    	String subject = claims.getSubject();
-    	return subject;
-    }
-    
-    public Date extractExpiration(String token) throws MalformedJwtException, ExpiredJwtException
-    {
-    	Claims claims = extractAllClaims(token);
-    	if (claims == null)
-    	{
-    		Calendar cal = Calendar.getInstance();
-    		cal.set(Calendar.YEAR, 1980);
-    		cal.set(Calendar.MONTH, Calendar.JANUARY);
-    		cal.set(Calendar.DAY_OF_MONTH, 1);
+	public String extractUsername(String token) throws MalformedJwtException, ExpiredJwtException {
+		Claims claims = extractAllClaims(token);
+		if (claims == null)
+			return null;
 
-    		Date dateRepresentation = cal.getTime();
-    		
-    		return dateRepresentation;
-    	}
+		String subject = claims.getSubject();
+		return subject;
+	}
 
-    	Date expirationDate = claims.getExpiration();
+	public Date extractExpiration(String token) throws MalformedJwtException, ExpiredJwtException {
+		Claims claims = extractAllClaims(token);
+		if (claims == null) {
+			Calendar cal = Calendar.getInstance();
+			cal.set(Calendar.YEAR, 1980);
+			cal.set(Calendar.MONTH, Calendar.JANUARY);
+			cal.set(Calendar.DAY_OF_MONTH, 1);
 
-    	return expirationDate;
-    }
-    
-    private Claims extractAllClaims(String token) throws MalformedJwtException, ExpiredJwtException
-    {
-		Claims claims = null; 
+			Date dateRepresentation = cal.getTime();
 
-		try
-		{
-			claims = Jwts.parser().setSigningKey(secretKey).build().parseSignedClaims(token).getPayload();
+			return dateRepresentation;
 		}
-		catch (SignatureException e)
-		{
-			System.out.println("signature exception" + e);
-		}
-		catch (MalformedJwtException e)
-		{
+
+		Date expirationDate = claims.getExpiration();
+
+		return expirationDate;
+	}
+
+	private Claims extractAllClaims(String token) throws MalformedJwtException, ExpiredJwtException {
+		Claims claims = null;
+
+		try {
+			claims = Jwts.parser().verifyWith(this.getSignInKey()).build().parseSignedClaims(token).getPayload();
+		} catch (MalformedJwtException e) {
 			System.out.println("token malformed" + e);
-		}
-		catch (ExpiredJwtException e)
-		{
+		} catch (ExpiredJwtException e) {
 			System.out.println("token expired" + e);
-		}
-		catch (UnsupportedJwtException e)
-		{
+		} catch (UnsupportedJwtException e) {
 			System.out.println("unsupported" + e);
-		}
-		catch (IllegalArgumentException e)
-		{
+		} catch (IllegalArgumentException e) {
 			System.out.println("Illegal" + e);
 		}
-		
+
 		return claims;
-    }
-    
-    public Boolean isTokenExpired(String token) throws MalformedJwtException, ExpiredJwtException
-    {
-    	return extractExpiration(token).before(new Date());
-    }
-    
-	public String generateToken(UserDetails userDetails)
-	{
+	}
+
+	public Boolean isTokenExpired(String token) throws MalformedJwtException, ExpiredJwtException {
+		return extractExpiration(token).before(new Date());
+	}
+
+	public String generateToken(UserDetails userDetails) {
 		Map<String, Object> claims = new HashMap<String, Object>();
-		
+
 		return this.createToken(claims, userDetails.getUsername());
 	}
-	
-	private String createToken(Map<String, Object> claims, String subject)
-	{
-		
-		String outputString = Jwts.builder().setClaims(claims).setSubject(subject)
-				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * this.jwtExpirationIntervalMinutes))
-				.signWith(SignatureAlgorithm.HS256, secretKey).compact();
-		
+
+	private String createToken(Map<String, Object> claims, String subject) {
+
+		String outputString = Jwts.builder().claims(claims).subject(subject)
+				.issuedAt(new Date(System.currentTimeMillis()))
+				.expiration(new Date(System.currentTimeMillis() + 1000 * 60 * this.jwtExpirationIntervalMinutes))
+				.signWith(this.getSignInKey()).compact();
+
 		return outputString;
 	}
 	
-	public Boolean validateToken(String token, UserDetails userDetails)
-	{
+	private SecretKey getSignInKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(this.secretKey);
+		
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+
+	public Boolean validateToken(String token, UserDetails userDetails) {
 		String username = StringUtils.EMPTY;
 		boolean isExpired = true;
-		
+
 		username = extractUsername(token);
 		if (username == null)
 			return false;
@@ -134,7 +121,6 @@ public class JwtUtil
 			return false;
 
 		return true;
-		
+
 	}
 }
-        
